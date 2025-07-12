fad0a168d1702ca77ee3f05022b1232532a41ac7
Francisco Ríos <panchorios_242@hotmail.com>
2025-07-12 00:03:11 -0400

    Added useRecommendations to show popular products

--- src/features/user/pages/UserHome.tsx
+++ src/features/user/pages/UserHome.tsx
@@ -1,8 +1,22 @@
+import ProductCard from "@/features/products/components/ProductCard";
+import useRecommendations from "@/features/recommendation/hooks/useRecommendation";
+
 const UserHome = () => {
+  const { products } = useRecommendations();
+
   return (
-    <div>
-      <h1>Cualquier cosa</h1>
-    </div>
+    <section className=" mx-auto max-w-7xl py-8">
+      <h2 className="text-2xl font-semibold mb-8 text-center">
+        Productos más populares
+      </h2>
+      {products?.length && (
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} viewMode="grid" />
+          ))}
+        </div>
+      )}
+    </section>
   );
 };
 
