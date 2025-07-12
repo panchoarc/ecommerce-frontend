b2a1da20f2c5ca91ce58580fd27b4a1299bc8e86
Francisco Ríos <panchorios_242@hotmail.com>
2025-07-12 00:04:50 -0400

    Add similar products to Product Details

--- src/features/products/pages/ProductDetails.tsx
+++ src/features/products/pages/ProductDetails.tsx
@@ -12,11 +12,15 @@ import FullscreenLoader from "@/shared/ui/ScreenLoader";
 import useProductReviews from "@/features/products/hooks/useProductReviews";
 import ProductReviews from "@/features/products/components/ProductReviews";
 import { showCartToast } from "@/features/products/components/addToCartToast";
-import ProductDescription from "../components/ProductDescription";
+import ProductDescription from "@/features/products/components/ProductDescription";
+import useSimilarProducts from "@/features/recommendation/hooks/useSimilarProducts";
+import ProductCard from "@/features/products/components/ProductCard";
 
 const ProductDetail: FC = () => {
   const { id } = useParams<{ id: string }>();
   const { addToCart } = useCart();
+
+  const { products: similarProducts } = useSimilarProducts(Number(id));
 
   const navigate = useNavigate();
   const {
@@ -75,8 +79,25 @@ const ProductDetail: FC = () => {
       <section className="my-8">
         <ProductDescription description={product.description} />
         <ProductReviews reviews={reviews} />
-      </section>
 
+        {similarProducts?.length != 0 && (
+          <section className=" mx-auto max-w-7xl py-8">
+            <h2 className="text-2xl font-semibold mb-8 text-center">
+              Productos Similares
+            </h2>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+              {similarProducts?.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  viewMode="grid"
+                />
+              ))}
+            </div>
+          </section>
+        )}
+      </section>
     </div>
   );
 };
