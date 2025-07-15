7b51ce5c5fa3a0d52ed44863d80372274f41500e
Francisco Ríos <panchorios_242@hotmail.com>
2025-07-14 22:49:27 -0400

    Fixed render problem in UserHome

--- src/features/user/pages/UserHome.tsx
+++ src/features/user/pages/UserHome.tsx
@@ -9,9 +9,9 @@ const UserHome = () => {
       <h2 className="text-2xl font-semibold mb-8 text-center">
         Productos más populares
       </h2>
-      {products?.length && (
-        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-          {products.map((product) => (
+      {products?.length != 0 && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+          {products?.map((product) => (
             <ProductCard key={product.id} product={product} viewMode="grid" />
           ))}
         </div>
